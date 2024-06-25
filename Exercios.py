d4796db2021478dc96478a3c040672116ec61043
Marcos Paulo Simões <75817190+DevWizardMarcos@users.noreply.github.com>
2024-06-25 18:21:45 -0300

    Add files via upload

---
+++ Exercios.py
@@ -0,0 +1,12 @@
+def calcular_area_retangulo(largura, altura):
+    return largura * altura # retora a altura e altura * = vs 
+
+# Atribuir diretamente os valores de largura e altura
+largura = 20
+altura = 40
+
+# Calcular a área juntando a largura e a a altura
+area = calcular_area_retangulo(largura, altura) # faz o calculo da Area
+
+
+print(f"A área do retângulo é: {area:.2f}") # Exibe o resultado
